4a714983991ac907a99398ca67c44491ab3dc379
cniblock <>
2023-12-13 00:18:07 +0000

    added js code for console logging popup-box title, description and date

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const addBox = document.querySelector(".add-box"),
     popupBox = document.querySelector(".popup-box"),
-    closeIcon = popupBox.querySelector("header i");
+    closeIcon = popupBox.querySelector("header i"),
+    titleTag = popupBox.querySelector("input"),
+    descTag = popupBox.querySelector("textarea"),
+    addBtn = popupBox.querySelector("button");
+
+const months = ["January", "February", "March", "April", "May", "June", "July",
+                 "August", "September", "October", "November", "December"];
 
 addBox.addEventListener("click", () => {
     popupBox.classList.add("show");
@@ -8,4 +14,23 @@ addBox.addEventListener("click", () => {
 
 closeIcon.addEventListener("click", () => {
     popupBox.classList.remove("show");
+});
+
+addBtn.addEventListener("click", e => {
+    e.preventDefault();
+    let noteTitle = titleTag.value,
+    noteDesc = descTag.value;
+
+    if(noteTitle || noteDesc ){
+        let dateObj = new Date(),
+        day = dateObj.getDate(),
+        month = months[dateObj.getMonth()],
+        year = dateObj.getFullYear();
+
+        let noteInfo = {
+            title: noteTitle, description: noteDesc,
+            date: `${day} ${month}, ${year}`
+        }
+        console.log(noteInfo);
+    }
 });
