38ce84724692f1666fecf85af3b51b7063a68c0f
cniblock <>
2023-12-13 12:40:15 +0000

    added js code to save notes to local storage

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const addBox = document.querySelector(".add-box"),
     addBtn = popupBox.querySelector("button");
 
 const months = ["January", "February", "March", "April", "May", "June", "July",
-                 "August", "September", "October", "November", "December"];
+    "August", "September", "October", "November", "December"];
 
 addBox.addEventListener("click", () => {
     popupBox.classList.add("show");
@@ -16,21 +16,46 @@ closeIcon.addEventListener("click", () => {
     popupBox.classList.remove("show");
 });
 
+function showNotes() {
+    notes.forEach((note) => {
+        let liTag = `<li class="note">
+        <div class="details">
+            <p>${note.title}</p>
+            <span>${note.description}</span>
+        </div>
+        <div class="bottom-content">
+            <span>${note.date}</span>
+            <div class="settings">
+                <i class="fa-solid fa-ellipsis"></i>
+                <ul class="menu">
+                        <li><i class="fa-regular fa-pen-to-square"></i>Edit</li>
+                        <li><i class="fa-regular fa-trash-can"></i>Delete</li>
+                </ul>
+            </div>
+        </div>
+        </li>`;
+        addBox.insertAdjacentHTML("afterend", liTag);
+    });
+}
+showNotes();
+
 addBtn.addEventListener("click", e => {
     e.preventDefault();
     let noteTitle = titleTag.value,
-    noteDesc = descTag.value;
+        noteDesc = descTag.value;
 
-    if(noteTitle || noteDesc ){
+    if (noteTitle || noteDesc) {
         let dateObj = new Date(),
-        day = dateObj.getDate(),
-        month = months[dateObj.getMonth()],
-        year = dateObj.getFullYear();
+            day = dateObj.getDate(),
+            month = months[dateObj.getMonth()],
+            year = dateObj.getFullYear();
 
         let noteInfo = {
             title: noteTitle, description: noteDesc,
             date: `${day} ${month}, ${year}`
         }
-        console.log(noteInfo);
+        const notes = [];
+        notes.push(noteInfo);
+        localStorage.setItem("notes", notes);
     }
 });
